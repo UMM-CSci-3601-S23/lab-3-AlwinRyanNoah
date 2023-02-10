f683b216bfdc73ac71479e8a3bf2940b38b121c3
Alwin Lor <lor00148@morris.umn.edu>
2023-02-09 20:21:12 -0600

    Create main todo files
    Still need to create the server filter for status

---
+++ client/src/app/todos/todo.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Todo, TodoCategory } from './todo';
+
+/*service that provides the interface for getting information
+about 'Todos' from the server*/
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  readonly todoUrl: string = environment.apiUrl + 'todos';
+
+
+  constructor(private httpClient: HttpClient) {
+   }
+
+   getTodos(filters?: {category?: TodoCategory; status?: boolean }): Observable<Todo[]> {
+    let httpParams: HttpParams = new HttpParams();
+    if (filters) {
+      if (filters.category) {
+        httpParams = httpParams.set('role', filters.category);
+      }
+      if (filters.status) {
+        httpParams = httpParams.set('age', filters.status.toString());
+      }
+    }
+    return this.httpClient.get<Todo[]>(this.todoUrl, {
+      params: httpParams,
+    });
+   }
+
+   getTodoById(id: string): Observable<Todo> {
+    return this.httpClient.get<Todo>(this.todoUrl + '/' + id);
+  }
+/*
+  filterUsers(todos: Todo[], filters?: { status?: boolean }): Todo[] {
+    let filteredTodos = todos;
+
+    if (filters.status) {
+      switch(String(filters.status).toLowerCase()){
+        case 'complete': {
+          return filteredTodos = filteredTodos.filter(todo => todo.status.toLowerCase().indexOf(filters.status) !== -1);
+        }
+      }
+    }
+}*/
+}
