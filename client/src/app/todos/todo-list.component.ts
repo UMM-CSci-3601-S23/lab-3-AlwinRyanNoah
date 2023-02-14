1d17a37b1726f896f648fdf5a8a7128cf82bf788
Ryan Sajulga <sajul004@morris.umn.edu>
2023-02-13 20:12:01 -0600

    Creating Filters for Todos
    Ryan Sajulga

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -41,9 +41,10 @@ export class TodoListComponent implements OnInit{
 
   public updateFilter() {
     this.filteredTodos = this.todoService.filterTodos(
-      this.serverFilteredTodo, { status: this.todoStatus }
+      this.serverFilteredTodo, { body: this.todoBody, category: this.todoCategory }
     );
   }
+
 
   ngOnInit(): void {
     this.getTodosFromServer();
