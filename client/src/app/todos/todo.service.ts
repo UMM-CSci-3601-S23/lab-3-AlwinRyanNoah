2fa6857d6e302b3578772dfe741979b73ab1bf3a
Alwin Lor <lor00148@morris.umn.edu>
2023-02-14 13:51:22 -0600

    fixed status filter
    change if to (filters.hasOwnProperty('status'))

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -25,12 +25,12 @@ export class TodoService {
       if (filters.owner) {
         httpParams = httpParams.set('owner', filters.owner);
       }
-      if (filters.status === true) {
+      if (filters.hasOwnProperty('status')) {
         httpParams = httpParams.set('status', filters.status.toString());
       }
-      if (filters.status === false) {
+      /*if (filters.status === false) {
         httpParams = httpParams.set('status', filters.status.toString());
-      }
+      }*/
     }
     return this.httpClient.get<Todo[]>(this.todoUrl, {
       params: httpParams,
