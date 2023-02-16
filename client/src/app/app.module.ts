f04b747b5172f613db462c0e5bc81f41f5aeb0fc
Alwin Lor <lor00148@morris.umn.edu>
2023-02-15 21:12:06 -0600

    Fix the merge
    Got rid of todo card/profile
    fix html changes that didn't carry over

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -31,8 +31,6 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { UserCardComponent } from './users/user-card.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { TodoListComponent } from './todos/todo-list.component';
-import { TodoCardComponent } from './todos/todo-card.component';
-import { TodoProfileComponent } from './todos/todo-profile.component';
 
 const MATERIAL_MODULES: any[] = [
   MatListModule,
@@ -61,8 +59,6 @@ const MATERIAL_MODULES: any[] = [
     UserCardComponent,
     UserProfileComponent,
     TodoListComponent,
-    TodoCardComponent,
-    TodoProfileComponent,
 
   ],
   imports: [
